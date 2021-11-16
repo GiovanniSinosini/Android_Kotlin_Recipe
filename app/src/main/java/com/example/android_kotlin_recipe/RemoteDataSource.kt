1b6194c7fe868ab6ca978efff121ed5ea5cbb003
gitName <carvalho.sino@gmail.com>
2021-11-16 22:28:42 +0000

    Inject RemoteDataSource class

--- app/src/main/java/com/example/android_kotlin_recipe/RemoteDataSource.kt
+++ app/src/main/java/com/example/android_kotlin_recipe/RemoteDataSource.kt
@@ -1,5 +1,14 @@
 package com.example.android_kotlin_recipe
 
-class RemoteDataSource() {
+import com.example.android_kotlin_recipe.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
 
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
