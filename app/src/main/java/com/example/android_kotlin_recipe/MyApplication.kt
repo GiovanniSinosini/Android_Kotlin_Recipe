5cca9809ed2307c05b91b1d598c1bb0099282f34
gitName <carvalho.sino@gmail.com>
2021-11-19 19:43:42 +0000

    Fixed bug - project rebuild

--- app/src/main/java/com/example/android_kotlin_recipe/MyApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.android_kotlin_recipe
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyApplication: Application() {
-}
--- app/src/main/java/com/example/android_kotlin_recipe/models/FoodRecipe.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android_kotlin_recipe.models
-
-
-import com.google.gson.annotations.SerializedName
-
-data class FoodRecipe(
-    @SerializedName("results")
-    val results: List<Result>
-)
