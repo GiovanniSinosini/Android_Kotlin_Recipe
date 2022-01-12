aecd60b037a71c3bc7a7f6e00753be446c588b6e
gitName <carvalho.sino@gmail.com>
2022-01-12 12:29:32 +0000

    Modify localDataSource for favoritesRecipes implementation

--- app/src/main/java/com/example/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/foody/viewmodels/MainViewModel.kt
@@ -22,7 +22,7 @@ class MainViewModel @ViewModelInject constructor(
 
     /** ROOM DATABASE - offline cache */
 
-    val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readDatabase().asLiveData()
+    val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readRecipes().asLiveData()
 
     private fun insertRecipes(recipesEntity: RecipesEntity) =
         viewModelScope.launch(Dispatchers.IO) {
